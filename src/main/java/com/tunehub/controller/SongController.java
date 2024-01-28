88fed41ff50882e22b90256c62043f15fc33d8f6
DANDE VENKATESH <DANDE VENKATESH@VENKATESH>
2024-01-28 21:24:21 +0530

    1st version of tune hub

---
+++ src/main/java/com/tunehub/controller/SongController.java
@@ -0,0 +1,61 @@
+package com.tunehub.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tunehub.entities.Song;
+import com.tunehub.services.SongService;
+@Controller
+public class SongController
+{
+	@Autowired
+	SongService service;
+	@PostMapping("/addSong")
+	public String addSong(@ModelAttribute Song song)
+	{
+		boolean songStatus=service.songExists(song.getName());
+		if(songStatus==false)
+		{
+			service.addSong(song);
+			System.out.println("Song is added succesfully");
+		}
+		else
+		{
+			System.out.println("song already exists");
+		}
+		
+		return "adminHome";
+	}
+	@GetMapping("/viewSongs")
+	public String viewSongs(Model model)
+	{
+		List<Song>songList=service.fetchAllSongs();
+		System.out.println(songList);
+		model.addAttribute("songs",songList);
+		return "displaySongs";
+	}
+	
+	@GetMapping("/playSongs")
+	public String playSongs(Model model)
+	{
+		boolean premiumUser=true;
+		if(premiumUser==true)
+		{
+			List<Song>songList=service.fetchAllSongs();
+			System.out.println(songList);
+			model.addAttribute("songs",songList);
+			return "displaySongs";
+		}
+		else
+		{
+			return "makePayment";
+		}
+	}
+
+}
---
+++ src/main/java/com/tunehub/services/PlaylistService.java
@@ -0,0 +1,12 @@
+package com.tunehub.services;
+
+import java.util.List;
+
+import com.tunehub.entities.Playlist;
+
+public interface PlaylistService
+{
+	public void addPlaylist(Playlist playlist);
+	public List<Playlist>fetchAllPlaylists();
+
+}
